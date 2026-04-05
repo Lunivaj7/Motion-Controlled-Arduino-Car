663f83d78f11f1e9deab7e24c2ecea097a29efda
Arian34580 <arypatel993@gmail.com>
2026-04-05 16:22:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,72 +3,129 @@ import mediapipe as mp
 import time
 from subprocess import call
 import numpy as np
-from Positions import HandGestureController
-##from arduino_manager import ArduinoManager
-import math
-gestureController = HandGestureController()
-time.sleep(2) 
-# 1. Initialize Arduino (Defaults to COM5 based on your class)
-##arduino = ArduinoManager()
-##arduino.connect()
+from arduino_manager import ArduinoManager
 
+#Python: Select Interperter: Python_Project\venv\Scripts\python.exe
+
+# 1. Initialize Arduino
+arduino = ArduinoManager()
+arduino.connect()
 
 # MediaPipe Setup
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = hands = mpHands.Hands(
+    max_num_hands=1,
+    model_complexity=1,
+    min_detection_confidence=0.8,
+    min_tracking_confidence=0.8
+)
 mpDraw = mp.solutions.drawing_utils
-landmark_style = mpDraw.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4)
+landmark_style = mpDraw.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=3)
 connection_style = mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2)
 
-VOLUME_UPDATE_INTERVAL = 20
 
-gestureController = HandGestureController()
+fingerTips_ID = [4,8,12,16,20] #[thumb, index, middle, ring, pinky]
 
 
-videoCap = cv2.VideoCapture(0)
+#video capture
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 lastFrameTime = 0
 frame_count = 0
-max_diff = 0
-min_diff = 100000
-lastState = "OFF"
+process_frames = 2
+process_data_frames = 10
 
+def get_hand_arr(handLms, img):
+    h, w, c = img.shape
+    lm = []
+    for i in range(21):
+        x = int(handLms.landmark[i].x * w)
+        y = int(handLms.landmark[i].y * h)
+        lm.append((x, y))
+    
+    #hand array: 1 = open, 0 = closed
+    #[thumb, index, middle, ring, pinky]
+    hand = [0, 0, 0, 0, 0]
 
+    #thumb
+    if lm[4][0] < lm[3][0]:
+        hand[0]=1
+    else:
+        hand[0]=0
+    
+    #other fingers
+    for i, tip in enumerate(fingerTips_ID[1:]):
+
+        if lm[tip][1]<lm[tip-2][1]:
+            hand[i+1] = 1
+        else:
+            hand[i+1] = 0
+    
+    return hand
+        
+#Video Loop
 while True:
-    frame_count += 1
-    success, img = videoCap.read()
-   
-    if success:
-        img = cv2.flip(img, 1)
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-       
-        thisFrameTime = time.time()
-        fps = 1 / (thisFrameTime - lastFrameTime)
-        lastFrameTime = thisFrameTime
-        cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    success, img = cap.read()
+    if not success:
+        continue
+    
+    #flips camera
+    img = cv2.flip(img, 1) 
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    #FPS calculation
+    thisFrameTime = time.time()
+    frame_dif = thisFrameTime - lastFrameTime
+    if (frame_dif!=0):
+        fps = 1 / (frame_dif)
+    else:
+        fps = 0
+    lastFrameTime = thisFrameTime
+    cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    frame_count+=1
+
+    #process frames 
+    if frame_count % process_frames == 0:
 
         results = hands.process(imgRGB)
-       
+
+        #hand calculations
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
                 mpDraw.draw_landmarks(
-                    img,
-                    handLms,
+                    img, 
+                    handLms, 
                     mpHands.HAND_CONNECTIONS,
                     landmark_style,
                     connection_style
                 )
-                if frame_count % VOLUME_UPDATE_INTERVAL == 0:
-                    state = gestureController.process(handLms, img.shape)
-                    frame_count = 0
 
+                #get hand array
+            if (frame_count%process_data_frames==0):
+                hand = get_hand_arr(handLms, img)    
+                print("Hand Array", hand)
 
-        cv2.imshow("CamOutput", img)
+                if hand==[1,1,1,1,1]:
+                    arduino.send("FWD")
+
+                elif hand==[1,0,0,0,0]:
+                    arduino.send("REV")
+            
+                elif hand==[0,1,0,0,0]:
+                    arduino.send("LEFT")
+            
+                elif hand==[0,0,0,0,1]:
+                    arduino.send("RIGHT")
+            
+        #renders camera
+        cv2.imshow("Hand Control", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-
-# 4. Clean up connection
-#arduino.close()
-videoCap.release()
+#close connections
+arduino.close()
+cap.release()
 cv2.destroyAllWindows()
